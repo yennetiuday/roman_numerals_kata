188965099b5927bd33655b8cda5dfca7b24be8d3
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-02-09 09:38:33 +0000

    Added testcase to check for restriction of numbers over 3000 changed code accordingly and refactored it.

--- src/main/java/com/techreturners/romanNumbers/RomanNumralsConverter.java
+++ src/main/java/com/techreturners/romanNumbers/RomanNumralsConverter.java
@@ -3,6 +3,9 @@ package com.techreturners.romanNumbers;
 public class RomanNumralsConverter {
 
     public String convert(int number) {
+    	if(number <1 || number >3000)
+    		throw new IllegalArgumentException("Number should be between 1 and 3000");
+    	
     	int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] romanNumbers = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     	
--- src/test/java/com/techreturners/romanNumbers/RomanNumralsConverterTest.java
+++ src/test/java/com/techreturners/romanNumbers/RomanNumralsConverterTest.java
@@ -1,128 +1,138 @@
 package com.techreturners.romanNumbers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RomanNumralsConverterTest {
+	
+	private RomanNumralsConverter converter;
+	
+	@BeforeEach
+	public void setUp() {
+		converter = new RomanNumralsConverter();
+	}
 
     @Test
     public void convertOneToNumeral(){
-        RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("I", converter.convert(1));
     }
 
     @Test
     public void convertTwoToNumeral(){
-        RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("II", converter.convert(2));
     }
     
     @Test
     public void convertFiveToNumeral(){
-        RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("V", converter.convert(5));
     }
     
     @Test
     public void convertFourToNumeral(){
-        RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("IV", converter.convert(4));
     }
     
     @Test
     public void convertEightToNumeral(){
-        RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("VIII", converter.convert(8));
     }
     
     @Test
     public void convertTenToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("X", converter.convert(10));
     }
     
     @Test
     public void convertNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("IX", converter.convert(9));
     }
     
     @Test
     public void convertThirtyNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("XXXIX", converter.convert(39));
     }
     
     @Test
     public void convertFortyToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("XL", converter.convert(40));
     }
     
     @Test
     public void convertFiftyToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("L", converter.convert(50));
     }
     
     @Test
     public void convertEightyNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("LXXXIX", converter.convert(89));
     }
     
     @Test
     public void convertNintyToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("XC", converter.convert(90));
     }
     
     @Test
+    public void convertNintyFiveToNumeral() {
+        assertEquals("XCV", converter.convert(95));
+    }
+    
+    @Test
     public void convertHundredToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("C", converter.convert(100));
     }
     
     @Test
     public void convertThreeNintyNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("CCCXCIX", converter.convert(399));
     }
 
     @Test
     public void convertFourHundredToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("CD", converter.convert(400));
     }
     
     @Test
     public void convertFiveHundredToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("D", converter.convert(500));
     }
     
     @Test
     public void convertEightNintyNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("DCCCXCIX", converter.convert(899));
     }
 
     @Test
     public void convertNineHundredToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("CM", converter.convert(900));
     }
     
     @Test
+    public void convertNineHundredFiftyToNumeral() {
+        assertEquals("CML", converter.convert(950));
+    }
+    
+    @Test
     public void convertThousandToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("M", converter.convert(1000));
     }
     
     @Test
     public void convertThousandNintyNineToNumeral() {
-    	RomanNumralsConverter converter = new RomanNumralsConverter();
         assertEquals("MXCIX", converter.convert(1099));
+    }
+    
+    @Test
+    public void convertThreeThousandToNumeral() {
+        assertEquals("MMM", converter.convert(3000));
+    }
+    
+    @Test
+    public void convertThreeThousandNineNintyNineToNumeral() {
+    	IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> converter.convert(3999));
+        assertEquals("Number should be between 1 and 3000", exception.getMessage());
     }
 }
